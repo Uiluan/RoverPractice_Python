bf62eeb26629fe2df3d8e9f7b2a772b5b85121a8
Uiluan <larsen.rcl@gmail.com>
2022-08-05 22:19:30 -0500

    Add movement wrap around

--- RoverApp/Rover.py
+++ RoverApp/Rover.py
@@ -6,17 +6,47 @@ class Rover():
         self.x = x
         self.direction = direction
 
+    def __GetDirectionString(self):
+        return {
+            const.NORTH: "N",
+            const.EAST: "E",
+            const.SOUTH: "S",
+            const.WEST: "W"
+        } [self.direction]
+
+    def __MoveNorth(self):
+        if(self.y == const.Y_MAX):
+            self.y = const.Y_MIN
+        else:
+            self.y += 1
+
+    def __MoveEast(self):
+        if(self.x == const.Y_MAX):
+            self.x = const.Y_MIN
+        else:
+            self.x += 1
+
+    def __MoveSouth(self):
+        if(self.y == const.Y_MIN):
+            self.y = const.Y_MAX
+        else:
+            self.y -= 1
+
+    def __MoveWest(self):
+        if(self.x == const.X_MIN):
+            self.x = const.Y_MAX
+        else:
+            self.x -= 1
+
     def ExecuteCommand(self, command: str) -> str:
         if(command == const.MOVE_COMMAND):
             if(self.direction == const.NORTH):
-                self.y += 1
+                self.__MoveNorth()
             elif(self.direction == const.EAST):
-                self.x += 1
+                self.__MoveEast()
+            elif(self.direction == const.SOUTH):
+                self.__MoveSouth()
+            elif(self.direction == const.WEST):
+                self.__MoveWest()
         
         return str(self.x) + ":" + str(self.y) + ":" + self.__GetDirectionString()
-
-    def __GetDirectionString(self):
-        if(self.direction == const.NORTH):
-            return "N"
-        elif(self.direction == const.EAST):
-            return "E"
--- Tests/test_Rover.py
+++ Tests/test_Rover.py
@@ -3,16 +3,50 @@ import random
 import RoverApp.Constants as const
 from RoverApp.Rover import Rover
 
-def test_ExecuteCommand_StartAt_00N_Given_M_EndsAt_01N():
-    rover = Rover(0, 0, const.NORTH)
-
+def test_ExecuteCommand_StartAt_55N_Given_M_EndsAt_56N():
+    rover = Rover(5, 5, const.NORTH)
     actual = rover.ExecuteCommand("M")
-    expected = "0:1:N"
-    assert actual == expected, f"Expected: {expected}, got: {actual}"
+    expected = "5:6:N"
+    assert actual == expected
 
-def test_ExecuteCommand_StartAt_00E_Given_M_EndsAt_10N():
-    rover = Rover(0, 0, const.EAST)
-
+def test_ExecuteCommand_StartAt_55E_Given_M_EndsAt_65N():
+    rover = Rover(5, 5, const.EAST)
     actual = rover.ExecuteCommand("M")
-    expected = "1:0:E"
-    assert actual == expected, f"Expected: {expected}, got: {actual}"
+    expected = "6:5:E"
+    assert actual == expected
+
+def test_ExecuteCommand_StartAt_55S_Given_M_EndsAt_54S():
+    rover = Rover(5, 5, const.SOUTH)
+    actual = rover.ExecuteCommand("M")
+    expected = "5:4:S"
+    assert actual == expected
+
+def test_ExecuteCommand_StartAt_55W_Given_M_EndsAt_45W():
+    rover = Rover(5, 5, const.WEST)
+    actual = rover.ExecuteCommand("M")
+    expected = "4:5:W"
+    assert actual == expected
+
+def test_ExecuteCommand_StartAt_00S_Given_M_EndsAt_09S():
+    rover = Rover(0, 0, const.SOUTH)
+    actual = rover.ExecuteCommand("M")
+    expected = "0:9:S"
+    assert actual == expected
+
+def test_ExecuteCommand_StartAt_09N_Given_M_EndsAt_00N():
+    rover = Rover(0, 9, const.NORTH)
+    actual = rover.ExecuteCommand("M")
+    expected = "0:0:N"
+    assert actual == expected
+
+def test_ExecuteCommand_StartAt_00W_Given_M_EndsAt_90W():
+    rover = Rover(0, 0, const.WEST)
+    actual = rover.ExecuteCommand("M")
+    expected = "9:0:W"
+    assert actual == expected
+
+def test_ExecuteCommand_StartAt_90E_Given_M_EndsAt_00E():
+    rover = Rover(9, 0, const.EAST)
+    actual = rover.ExecuteCommand("M")
+    expected = "0:0:E"
+    assert actual == expected
